68c8167d298cee4a3f8af5e37cb9d1f0d266cf16
Alex Gallant <agallant1201@gmail.com>
2019-11-03 14:28:45 -0700

    Edit shiftable module

--- module/shiftable.rb
+++ module/shiftable.rb
@@ -1,4 +1,4 @@
-class Shiftable
+module Shiftable
 
   def create_shift(key, date)
   keys = key_shift(key)
@@ -22,12 +22,12 @@ class Shiftable
   end
 
   def date_shift(date)
-  date_squared = (date.to_i ** 2).to_s
-  a = date_squared[-4].to_i
-  b = date_squared[-3].to_i
-  c = date_squared[-2].to_i
-  d = date_squared[-1].to_i
+    date_squared = (date.to_i ** 2).to_s
+    a = date_squared[-4].to_i
+    b = date_squared[-3].to_i
+    c = date_squared[-2].to_i
+    d = date_squared[-1].to_i
 
-  [a, b, c, d]
+    [a, b, c, d]
   end
 end
