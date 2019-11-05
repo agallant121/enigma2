75b039043add6454a87002f2ca5ffac01d90f13d
Alex Gallant <agallant1201@gmail.com>
2019-11-05 15:19:15 -0700

    Work on encryption and decryption bugs

---
+++ lib/decryption.rb
@@ -0,0 +1,23 @@
+class Decryption
+
+
+  def decrypt(message, key, date)
+    alphabet = ("a".."z").to_a << " "
+    shift = Shift.create_shift(key, date)
+
+    message.downcase.chars.map do |letter|
+
+      if letter == " "
+        actual = alphabet[(26 - shift[0])%27]
+        shift.rotate!
+        actual
+      elsif !alphabet.include?(letter)
+        actual = letter
+      else
+        actual = alphabet[(letter.ord-97 - shift[0])%27]
+        shift.rotate!
+        actual
+      end
+    end.join
+  end
+end
--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -1,11 +1,12 @@
 require "./test/test_helper"
 require 'date'
+require './lib/encryption'
 require './lib/decryption'
 
 class DecryptionTest < Minitest::Test
 
-  def test_it_returns_encrypted_message
+  def test_it_returns_decrypted_message
     encryption = Encryption.new
-    assert_equal "hello world", encryption.encrypt("keder ohulw", "02715", "040895")
+    assert_equal "hello world", encryption.decrypt("keder ohulw", "02715", "040895")
   end
 end
